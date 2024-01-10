5ac63e95f2cd70fd1abc87deb5fe027d0a4ed27c
almondnick <nickalmond14@gmail.com>
2024-01-10 13:22:41 -0600

    changes to file structure and added necessary files for dashboard, comment, viewing post, and editing post

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,4 +2,10 @@ module.exports = {
     format_date: (date) => {
         return date.toLocaleDateString();
     },
-}
+    format_time: (date) => {
+        return date.toLocaleTimeString();
+    },
+    format_datetime: (date) => {
+        return date.toLocaleTimeString() + ' on ' + date.toLocaleDateString();
+    },
+};
